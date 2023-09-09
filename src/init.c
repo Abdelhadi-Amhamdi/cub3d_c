fb35314990cf03f0ab98479b972117d6b600f14e
Abdellah Agouzoul <aagouzou@e3r3p3.1337.ma>
2023-09-09 16:35:06 +0100

    just testing

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: aagouzou <aagouzou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 11:41:47 by aagouzou          #+#    #+#             */
-/*   Updated: 2023/09/08 11:26:22 by aagouzou         ###   ########.fr       */
+/*   Updated: 2023/09/09 13:34:42 by aagouzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	init_data(t_map_data *data)
 	set_plyr_attr(data);
 	data->texture = mlx_load_png("./sci-fi.png");
 	data->num_rays = data->win_width;
-	data->mlx = mlx_init(data->win_width,data->win_height, "cub3d", 1);
+	data->mlx = mlx_init(data->win_width,data->win_height, "cub3d", 0);
 	if(!data->mlx)
 		ft_error("error: failed to init mlx");
 	data->img = mlx_new_image(data->mlx, data->win_width, data->win_height);
