16815516d72b545b224102ed242db7791a8fa33d
Abdelhadi Amhamdi <aamhamdi@e3r6p5.1337.ma>
2023-09-11 16:50:06 +0100

    [add costum map]

--- src/mini_map.c
+++ src/mini_map.c
@@ -6,11 +6,40 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 15:49:46 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/09/10 21:20:47 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/09/11 16:24:29 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
+void	_draw_costum_map(t_data *data, t_mini_map map)
+{
+	int		y;
+	int		x;
+	t_rect	rect;
+	int		tmp;
+
+	y = 0;
+	tmp = map.start_x;
+	while (map.start_y < map.end_y)
+	{
+		x = 0;
+		map.start_x = tmp;
+		while (map.start_x < map.end_x)
+		{
+			if (data->map_data->map_body[map.start_y][map.start_x] == '1')
+			{
+				rect = init_rect(x, y, CUB_SIZE * (float)MINI_MAP, \
+				data->map_data->map_body[map.start_y][map.start_x]);
+				render_rect(rect, data);
+			}
+			map.start_x++;
+			x++;
+		}
+		map.start_y++;
+		y++;
+	}
+}
 
 int	render_rect(t_rect rect, t_data *data)
 {
@@ -53,35 +82,56 @@ t_rect	init_rect(int x, int y, int size, char item)
 	return (rect);
 }
 
-// void	calc_start_and_end(t_game *game)
-// {
-// 	game->data->start_y = (game->player->y / ITEM_SIZE) - COSTUM_SIZE;
-// 	if (game->data->start_y < 0)
-// 		game->data->start_y = 0;
-// 	game->data->end_y = (game->player->y / ITEM_SIZE) + COSTUM_SIZE;
-// 	if (game->data->end_y > game->data->rows)
-// 		game->data->end_y = game->data->rows;
-// 	game->data->start_x = (game->player->x / ITEM_SIZE) - COSTUM_SIZE;
-// 	if (game->data->start_x < 0)
-// 		game->data->start_x = 0;
-// 	game->data->end_x = (game->player->x / ITEM_SIZE) + COSTUM_SIZE;
-// 	if (game->data->end_x > game->data->cols)
-// 		game->data->end_x = game->data->cols;
-// }
+void	calc_start_and_end(t_mini_map *map, t_data *data)
+{
+	map->start_y = (data->p_data->player_y / CUB_SIZE) - COSTUM_SIZE;
+	if (map->start_y < 0)
+		map->start_y = 0;
+	map->end_y = (data->p_data->player_y / CUB_SIZE) + COSTUM_SIZE;
+	if (map->end_y > data->map_data->rows)
+		map->end_y = data->map_data->rows;
+	map->start_x = (data->p_data->player_x / CUB_SIZE) - COSTUM_SIZE;
+	if (map->start_x < 0)
+		map->start_x = 0;
+	map->end_x = (data->p_data->player_x / CUB_SIZE) + COSTUM_SIZE;
+	if (map->end_x > data->map_data->cols)
+		map->end_x = data->map_data->cols;
+}
 
-void	_draw_player(t_data *data)
+void	mini_map_helper(t_data *data)
+{
+	t_mini_map mini_map;
+
+	if (data->map_type == NORMAL_MAP)
+		_draw_map(data->map_data, data);
+	else if (data->map_type == COSUTM_MAP)
+	{
+		calc_start_and_end(&mini_map, data);
+		_draw_costum_map(data, mini_map);
+		_draw_player(data, &mini_map);
+	}
+}
+
+void	_draw_player(t_data *data, t_mini_map *map)
 {
 	int		x;
 	int		y;
 	t_rect	rect;
 
-
-	x = data->p_data->player_x * (float)MINI_MAP;
-	y = data->p_data->player_y * (float)MINI_MAP;
-	
+	if (data->map_type == COSUTM_MAP)
+	{
+		x = (((data->p_data->player_x / CUB_SIZE) - map->start_x) * CUB_SIZE) * MINI_MAP;
+		y = (((data->p_data->player_y / CUB_SIZE) - map->start_y) * CUB_SIZE) * MINI_MAP;
+	}
+	else
+	{
+		x = data->p_data->player_x * (float)MINI_MAP;
+		y = data->p_data->player_y * (float)MINI_MAP;
+	}
 	rect = init_rect(x, y, 4 * MINI_MAP, 'P');
 	render_rect(rect, data);
-	draw_line(data, x, y, x + cos(data->p_data->player_Angle) * 15, y + sin(data->p_data->player_Angle) * 15);
+	draw_line(data, x + 2 , y + 2 , x + 2 + cos(data->p_data->player_Angle) \
+	* 15, y + 2 + sin(data->p_data->player_Angle) * 15);
 }
 
 void	_draw_map(t_map_data *m_data, t_data *data)
@@ -107,34 +157,7 @@ void	_draw_map(t_map_data *m_data, t_data *data)
 		}
 		y++;
 	}
-	_draw_player(data);
+	_draw_player(data, NULL);
 }
 
-// void	_draw_costum_map(t_data *data, t_game *game)
-// {
-// 	int		y;
-// 	int		x;
-// 	t_rect	rect;
-// 	int		tmp;
 
-// 	y = 0;
-// 	tmp = data->start_x;
-// 	while (data->start_y < data->end_y)
-// 	{
-// 		x = 0;
-// 		game->data->start_x = tmp;
-// 		while (data->start_x < data->end_x)
-// 		{
-// 			if (data->map->map_body[data->start_y][data->start_x] == '1')
-// 			{
-// 				rect = init_rect(x, y, ITEM_SIZE * (float)MINI_MAP, \
-// 				data->map->map_body[data->start_y][data->start_x]);
-// 				render_rect(rect, game);
-// 			}
-// 			game->data->start_x++;
-// 			x++;
-// 		}
-// 		game->data->start_y++;
-// 		y++;
-// 	}
-// }
